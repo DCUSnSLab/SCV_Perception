c59c50bf6f4ff0f942be87c18718b5c654c2e4b6
jjs523 <jinseong523@gmail.com>
2025-09-08 18:33:26 +0900

    A better version

--- tl_roi_infer/setup.py
+++ tl_roi_infer/setup.py
@@ -5,20 +5,31 @@ package_name = 'tl_roi_infer'
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', glob('launch/*.py')),
-        ('share/' + package_name + '/models', glob('models/*')),
+        ('share/' + package_name + '/launch',  glob('launch/*.py')),
+        ('share/' + package_name + '/models',  glob('models/*')),
+        ('share/' + package_name + '/params',  glob('params/*.yaml')),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        'setuptools',
+        'ultralytics',
+        'torch',
+        'torchvision',
+        'opencv-python',
+        'numpy',
+    ],
     zip_safe=True,
     maintainer='jjs523',
     maintainer_email='jjs523@todo.todo',
     description='Traffic light ROI inference node (ROS2, YOLO)',
     license='MIT',
-    # scripts 방식으로 설치 (shebang = /usr/bin/env python3)
-    scripts=['scripts/roi_infer'],
+    entry_points={
+        'console_scripts': [
+            'roi_infer = tl_roi_infer.roi_infer:main',
+        ],
+    },
 )
